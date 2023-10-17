9a4e876a4b36d94cb8ab675543fa9a92554cf14a
Corsi.20188 <Corsi.20188@E2-9PCWS2>
2023-10-17 13:45:44 +0200

    commit 1

--- 7 - Esercitazione automobile/Form1.Designer.cs
+++ 7 - Esercitazione automobile/Form1.Designer.cs
@@ -28,20 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.accensione = new System.Windows.Forms.Button();
+            this.Accelerazione = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.prima = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.statoAccensione = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // accensione
+            // 
+            this.accensione.Location = new System.Drawing.Point(180, 213);
+            this.accensione.Name = "accensione";
+            this.accensione.Size = new System.Drawing.Size(109, 97);
+            this.accensione.TabIndex = 0;
+            this.accensione.Text = "Accensione";
+            this.accensione.UseVisualStyleBackColor = true;
+            this.accensione.Click += new System.EventHandler(this.accensione_Click);
+            // 
+            // Accelerazione
+            // 
+            this.Accelerazione.Location = new System.Drawing.Point(661, 354);
+            this.Accelerazione.Name = "Accelerazione";
+            this.Accelerazione.Size = new System.Drawing.Size(89, 190);
+            this.Accelerazione.TabIndex = 1;
+            this.Accelerazione.Text = "Accelerazione";
+            this.Accelerazione.UseVisualStyleBackColor = true;
+            this.Accelerazione.Click += new System.EventHandler(this.Accelerazione_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(557, 354);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 190);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Decelerazione";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(346, 52);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(613, 91);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // prima
+            // 
+            this.prima.Location = new System.Drawing.Point(877, 384);
+            this.prima.Name = "prima";
+            this.prima.Size = new System.Drawing.Size(62, 60);
+            this.prima.TabIndex = 4;
+            this.prima.Text = "1";
+            this.prima.UseVisualStyleBackColor = true;
+            this.prima.Click += new System.EventHandler(this.prima_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(945, 384);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(62, 60);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "2";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1013, 384);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(62, 60);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "3";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1081, 384);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(62, 60);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "4";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1149, 384);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(62, 60);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "5";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1217, 384);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(62, 60);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "6";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // statoAccensione
+            // 
+            this.statoAccensione.AutoSize = true;
+            this.statoAccensione.Location = new System.Drawing.Point(320, 250);
+            this.statoAccensione.Name = "statoAccensione";
+            this.statoAccensione.Size = new System.Drawing.Size(0, 13);
+            this.statoAccensione.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1308, 678);
+            this.Controls.Add(this.statoAccensione);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.prima);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Accelerazione);
+            this.Controls.Add(this.accensione);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button accensione;
+        private System.Windows.Forms.Button Accelerazione;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button prima;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label statoAccensione;
     }
 }
 
--- 7 - Esercitazione automobile/Form1.cs
+++ 7 - Esercitazione automobile/Form1.cs
@@ -5,16 +5,144 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace _7___Esercitazione_automobile
 {
+    
     public partial class Form1 : Form
     {
+        Automobile auto;
         public Form1()
         {
             InitializeComponent();
+            auto = new Automobile();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            statoAccensione.Text = "STATO: Spento";
+        }
+
+        private void Accelerazione_Click(object sender, EventArgs e)
+        {
+            auto.Accelerazione();
+            listView1.Clear();
+            listView1.Items.Add(Convert.ToString(auto.Velocita));
+        }
+
+        private void prima_Click(object sender, EventArgs e)
+        {
+            auto.marciaAuto(1);
+        }
+
+        private void accensione_Click(object sender, EventArgs e)
+        {
+            int stato = auto.accensioneAuto();
+            if (stato == 1)
+                statoAccensione.Text = "STATO: Acceso";
+            else
+                statoAccensione.Text = "STATO: Spento";
+
+
+        }
+    }
+
+    public class Automobile
+    {
+        // attributi
+        private int _velocita;
+        private int _marcia;
+        private int _accensione;
+
+        // costruttore
+        public Automobile()
+        {
+            Velocita = 0;
+            Marcia = 0;
+            Accensione = 0;
+        }
+
+        // metodi accessor
+        public int Velocita
+        {
+            get { return _velocita; }
+            set { _velocita = value; }
+        }
+        public int Marcia
+        {
+            get { return _marcia; }
+            set { _marcia = value; }
+        }
+        public int Accensione
+        {
+            get { return _accensione; }
+            set { _accensione = value; }
+        }
+
+        // metodi
+
+        public int accensioneAuto()
+        {
+            if (_accensione == 0 && _velocita == 0)
+                return _accensione = 1;
+            else
+            {
+                while (_velocita != 0)
+                {
+                    _velocita -= 5;
+                    Thread.Sleep(500);
+                }
+                return _accensione = 0;
+                
+            }
+        }
+
+        public void marciaAuto(int cambio)
+        {
+            if (_marcia + 1 == cambio)
+                _marcia = cambio;
+            else if (_marcia - 1 == cambio)
+                _marcia = cambio;
+        }
+        public void Accelerazione()
+        {
+            if(_accensione == 1)
+            {
+                if (_marcia == 1 && _velocita < 30)
+                    _velocita += 5;
+                else if (_marcia == 2 && _velocita < 60)
+                    _velocita += 5;
+                else if (_marcia == 3 && _velocita < 70)
+                    _velocita += 5;
+                else if (_marcia == 4 && _velocita < 90)
+                    _velocita += 5;
+                else if (_marcia == 5 && _velocita < 110)
+                    _velocita += 5;
+                else if(_marcia == 6 && _velocita < 130)
+                    _velocita += 5;
+            }
+        }
+        public void Decelerazione()
+        {
+            if (_accensione == 1)
+            {
+                if (_marcia == 1 && _velocita > 0)
+                    _velocita -= 5;
+                else if (_marcia == 2 && _velocita > 30)
+                    _velocita -= 5;
+                else if (_marcia == 3 && _velocita > 60)
+                    _velocita -= 5;
+                else if (_marcia == 4 && _velocita > 70)
+                    _velocita -= 5;
+                else if (_marcia == 5 && _velocita > 90)
+                    _velocita -= 5;
+                else if (_marcia == 6 && _velocita > 110)
+                    _velocita -= 5;
+            }
         }
     }
 }
